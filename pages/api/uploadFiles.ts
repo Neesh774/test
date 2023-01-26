60dd8403b245e3faa4771135894523177a69f277
Neesh774 <Kanishq0106@gmail.com>
2023-01-25 21:38:52 -0500

    uploadFiles route

---
+++ pages/api/uploadFiles.ts
@@ -0,0 +1,46 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { string, z } from "zod";
+import Cors from 'cors'
+import runMiddleware from "@/utils/runMiddleware";
+import { decode } from 'base64-arraybuffer'
+
+const cors = Cors({
+  methods: ['POST'],
+})
+
+
+// upload media(images, videos) to supabase storage for a post
+const uploadPostMedia: NextApiHandler = async (req, res) => {
+  await runMiddleware(req, res, cors)
+
+  const mediaSchema = z.object({
+    mediaURLs: z.array(z.string()),
+    post_id: z.string(),
+  })
+
+  const object = mediaSchema.safeParse(req.body);
+
+  if (!object.success) {
+    res.status(400).json({ message: "Invalid request body", error: object.error, date: null });
+    return;
+  }
+  const { mediaURLs, post_id } = object.data;
+
+  const client = new SupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.SUPABASE_SERVICE_KEY as string);
+  await Promise.all(mediaURLs.map(async (mediaURL) => {
+    const fileName = mediaURL.split('/').pop() as string;
+    const blob = await fetch(mediaURL).then(r => r.blob())
+    const { data, error } = await client.storage.from('post-media').upload(`${post_id}/${fileName}`, blob, {
+      contentType: blob.type,
+    });
+    if (error) {
+      res.status(500).json({ message: "Internal server error", error, data: null });
+      return;
+    }
+  }))
+
+  res.status(200).json({ message: "Success", error: null, data: null });
+}
+
+export default uploadPostMedia;
